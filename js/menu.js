da36c43e881ae4c8c0afd6144b2724dc8ff196c0
Luiz Felipe <btmluiz@outlook.com>
2019-10-09 18:46:29 -0300

    Add Viewport

--- js/menu.js
+++ js/menu.js
@@ -12,3 +12,23 @@ $(function() {
       return false; 
     })
 });
+
+$(function () {
+    var hamburger = $('.hamburger'),
+        $nav = $('#site-nav'),
+        $masthead = $('#masthead');
+
+    hamburger.mouseenter(function () {
+        $(this).addClass('is-active');
+        $nav.addClass('is-active');
+        $masthead.addClass('is-active');
+        return false;
+    });
+
+    $masthead.mouseleave(function () {
+        $(this).removeClass('is-active');
+        $(hamburger).removeClass('is-active');
+        $nav.removeClass('is-active');
+        return false;
+    });
+});
